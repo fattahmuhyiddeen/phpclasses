ac89962ea96371e72eedc477e88dc70ae873679c
Susan Lee <susanlee@Susans-iMac.local>
2016-12-29 11:22:08 +0800

    add general library

---
+++ LibraryController.php
@@ -0,0 +1,75 @@
+<?php
+//This namespace and use are following laravel Controllers
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use DateTime;
+use App\Http\Requests;
+
+class LibraryController extends Controller
+{
+
+    //This function is to show where php.ini file (which used by current webserver) located in the computer
+	public function getwhichphpiniisused(){
+        $inipath = php_ini_loaded_file();
+        if ($inipath) {
+            return 'Loaded php.ini: ' . $inipath;
+        } else {
+            return 'A php.ini file is not loaded';
+        }
+    }
+    
+    //This function is take datetime (in string datatype) as input and return how long the distance from now. for example 1 minute elapse
+    //another optional parameter is if true, it will return full, for example 1 month, 3 weeks and 4 days
+    function time_elapsed_string($datetime, $full = false) {
+	    $now = new DateTime;
+	    $ago = new DateTime($datetime);
+	    $diff = $now->diff($ago);
+
+	    $diff->w = floor($diff->d / 7);
+	    $diff->d -= $diff->w * 7;
+
+	    $string = array(
+	        'y' => 'year',
+	        'm' => 'month',
+	        'w' => 'week',
+	        'd' => 'day',
+	        'h' => 'hour',
+	        'i' => 'minute',
+	        's' => 'second',
+	    );
+	    foreach ($string as $k => &$v) {
+	        if ($diff->$k) {
+	            $v = $diff->$k . ' ' . $v . ($diff->$k > 1 ? 's' : '');
+	        } else {
+	            unset($string[$k]);
+	        }
+	    }
+
+	    if (!$full) $string = array_slice($string, 0, 1);
+	    return $string ? implode(', ', $string) . ' ago' : 'just now';
+	}
+
+    //return random string which is quite secure for generate random keys
+	public static function random_str($length, $keyspace = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    {
+        $str = '';
+        $max = mb_strlen($keyspace, '8bit') - 1;
+        for ($i = 0; $i < $length; ++$i) {
+            $str .= $keyspace[random_int(0, $max)];
+        }
+        return $str;
+    }
+
+    public function formatMilliseconds($milliseconds) {
+        $seconds = floor($milliseconds / 1000);
+        $minutes = floor($seconds / 60);
+        $hours = floor($minutes / 60);
+        $milliseconds = $milliseconds % 1000;
+        $seconds = $seconds % 60;
+        $minutes = $minutes % 60;
+
+        $format = '%u:%02u:%02u.%03u';
+        $time = sprintf($format, $hours, $minutes, $seconds, $milliseconds);
+        return rtrim($time, '0');
+    }
+}
